9b6c9ebde8855fd9a9d18316bf4a5043104e1d9f
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-04-08 22:17:48 +0800

    added while loop for menu, ques_count(), and fixed write formatting

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,23 +1,32 @@
 #import time
 import time
 
+#initialize new_line
+new_line = "\n"
+
 #define add_question(string: str, code: int): 
 def add_question(string: str, code: int):
     with open("questions.txt", "a") as file:
-        file.write(f"<{bin(code)}> <question>:{string}")
+        file.write(f"{new_line}<{bin(code)}> <question>:{string}")
         file.close()
 
 #define add_choices(string: str, code: int):
 def add_choices(string: str, code:int):
     with open("questions.txt", "a") as file:
-        file.write(f"<{bin(code)}> <choice>:{string}")
+        file.write(f"{new_line}<{bin(code)}> <choice>:{string}")
         file.close()
 
 #define add_correct(string: str, code: int):
 def add_correct(string: str, code: int):
     with open("questions.txt", "a") as file:
-        file.write(f"<{bin(code)}> <correct>:{string}")
+        file.write(f"{new_line}<{bin(code)}> <correct>:{string}")
         file.close()
+
+#define ques_count() -> int:
+def ques_count() -> int:
+    with open("questions.txt", "r") as file:
+        content = file.read()
+        return content.count("<question>")
 
 #define find_question(code: int):
 def find_question(code: int):
@@ -32,5 +41,30 @@ try:
         file.close()
 
 except FileExistsError:
-    print("questions.txt is already exists")
+    print("questions.txt is already created")
 
+#while loop to continue asking user for input until ended
+while True:
+    local_count = ques_count()
+
+    print(f"""Welcome to Quiz Creator! 
+
+Main Menu:
+
+Questions added: {local_count} 
+[1] Add Question Set
+[2] Quit""")
+    
+    while True:
+        user_select = input("Select from the menu above!: ")
+        if user_select.isnumeric() and int(user_select) < 3:
+            break
+        else:
+            continue
+
+    if user_select == "1":
+        user_input = input("Input the question you want to add: ")
+        add_question(user_input, local_count + 1)
+
+    elif user_select == "2":
+        break
