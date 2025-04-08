f42275f3a23681441e52b83c3b9f137e266a3962
Mharlee Dela Cruz <marwilsondc1204@gmail.com>
2025-04-08 21:33:10 +0800

    added function codes and initialize question.txt file feature

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,36 @@
+#import time
+import time
+
 #define add_question(string: str, code: int): 
+def add_question(string: str, code: int):
+    with open("questions.txt", "a") as file:
+        file.write(f"<{bin(code)}> <question>:{string}")
+        file.close()
+
 #define add_choices(string: str, code: int):
+def add_choices(string: str, code:int):
+    with open("questions.txt", "a") as file:
+        file.write(f"<{bin(code)}> <choice>:{string}")
+        file.close()
+
 #define add_correct(string: str, code: int):
+def add_correct(string: str, code: int):
+    with open("questions.txt", "a") as file:
+        file.write(f"<{bin(code)}> <correct>:{string}")
+        file.close()
+
 #define find_question(code: int):
+def find_question(code: int):
+    with open("questions.txt", "r") as file:
+        content = file.read()
+        return content.find(f"{bin(code)} <question>")
+
+#create the file 
+try:
+    with open("questions.txt", "x") as file: 
+        file.write(f"File created at: {time.asctime()}")
+        file.close()
+
+except FileExistsError:
+    print("questions.txt is already exists")
+
